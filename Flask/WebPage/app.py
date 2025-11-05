ace006f9a8bf939a54ce457615bbaefbd39f922c
AbhishekSingh321 <singhabhi292004@gmail.com>
2025-11-06 00:26:24 +0530

    Dealing with json file to serve the data from flask to webpage

--- Flask/WebPage/app.py
+++ Flask/WebPage/app.py
@@ -2,13 +2,16 @@ from flask import *
 
 app=Flask(__name__)
 
+with open('data.json',encoding='utf-8') as f:
+  data=json.load(f)
+
 @app.route('/<name>')
 def Profile(name):
-  data={
-    'age' : 21,
-    'class' : 'TE'
-  }
-  return render_template('index.html',n=name,d=data)
+  if name in data:
+    user=data[name]
+    return render_template('index.html',n=name,d=user)
+  else:
+    return render_template('error.html')
 
 if __name__=='__main__':
   app.run(debug=True,port=3157)
