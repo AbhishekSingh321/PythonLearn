d1fb73309bfad1245f452816b0656c16d12f3001
AbhishekSingh321 <singhabhi292004@gmail.com>
2026-01-11 00:01:50 +0530

    Adjust text-to-speech settings: set rate to 150 and volume to 1.0

--- TTS/tts1.py
+++ TTS/tts1.py
@@ -2,9 +2,9 @@ import pyttsx3
 
 engine = pyttsx3.init()
 
-rate = engine.getProperty('rate')
-engine.setProperty('rate', rate - 50)
-engine.setProperty('volume', 0.5)  # Range: 0.0 – 1.0
+# rate = engine.getProperty('rate')
+engine.setProperty('rate', 150)   # (Slow: 100–150) (Normal: 180–220) (Fast: 250–300)
+engine.setProperty('volume', 1.0)  # Range: 0.0 – 1.0
 
 voices = engine.getProperty('voices')
 for i, voice in enumerate(voices):
