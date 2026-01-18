987b7ec3c948bb8bdff4ba965fd7491f0cd1f3fc
AbhishekSingh321 <singhabhi292004@gmail.com>
2026-01-18 23:39:05 +0530

    add element-wise addition in Numpy

--- Numpy/app.py
+++ Numpy/app.py
@@ -2,5 +2,9 @@ import numpy as np
 
 arr=np.array([1,2,3,4,5])
 
-print(arr)
-print(type(arr))
+# print(arr)
+# print(type(arr))
+
+arr2=np.array([1,2,3,4,5])
+
+print(arr+arr2)  # Not concatenation, element-wise addition
